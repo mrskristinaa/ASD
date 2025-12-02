081d932c30c50e487ea092aa96145d742530167b
mrskristinaa <krispis2006@gmail.com>
2025-12-02 16:54:16 +0300

    Fix DSU and update tests

--- lib_dsu/dsu.h
+++ lib_dsu/dsu.h
@@ -1,40 +1,44 @@
+#pragma once
 class DSU {
-	int* _parent;
-	int _size;
-	int* _rank;
+    int* _parent;
+    int* _rank;
+    int _size;
 public:
-	DSU(int size);
-	void unite(int, int);
-	int find(int);
-	~DSU();
+    DSU(int size);
+    void unite(int, int);
+    int find(int);
+    ~DSU();
 };
 DSU::DSU(int size) : _size(size) {
-	_parent = new int[_size];
-	_rank = new int[size];
-	for (int i = 0; i < _size; i++) {
-		_parent[i] = i;
-		_rank[i] = 0;
-	}
+    _parent = new int[_size];
+    _rank = new int[_size];
+    for (int i = 0; i < _size; i++) {
+        _parent[i] = i;
+        _rank[i] = 0;
+    }
 }
-DSU:: ~DSU() {
-	delete[] _parent;
+DSU::~DSU() {
+    delete[] _parent;
+    delete[] _rank;  
 }
 int DSU::find(int x) {
-	if (_parent[x] == x) {
-		return x;
-	}
-	else {
-		return find(_parent[x]);
-	}
+    if (_parent[x] != x) {
+        _parent[x] = find(_parent[x]);  
+    }
+    return _parent[x];
 }
-void DSU::unite(int x1, int x2) {
-	if (_rank[x1] < _rank[x2]) {
-		_parent[x1] = find(x2);
-	}
-	else {
-		if (_rank[x1] == _rank[x2]) {
-			_rank[x1]++;
-		}
-		_parent[x2] = find(x1);
-	}
+void DSU::unite(int x, int y) {
+    int rootX = find(x);  
+    int rootY = find(y);
+    if (rootX == rootY) return;
+    if (_rank[rootX] < _rank[rootY]) {
+        _parent[rootX] = rootY;
+    }
+    else if (_rank[rootX] > _rank[rootY]) {
+        _parent[rootY] = rootX;
+    }
+    else {
+        _parent[rootY] = rootX;
+        _rank[rootX]++;
+    }
 }
--- tests/test_dsu.cpp
+++ tests/test_dsu.cpp
@@ -1,83 +1,46 @@
 #include <gtest/gtest.h>
 #include "dsu.h"
-TEST(DSUBasicTest, Initialization) {
+TEST(DSU_Simple, Create) {
     DSU dsu(5);
     EXPECT_EQ(dsu.find(0), 0);
-    EXPECT_EQ(dsu.find(1), 1);
-    EXPECT_EQ(dsu.find(2), 2);
-    EXPECT_EQ(dsu.find(3), 3);
     EXPECT_EQ(dsu.find(4), 4);
 }
-
-TEST(DSUBasicTest, SimpleUnion) {
-    DSU dsu(3);
-    dsu.unite(0, 1);
-    int root0 = dsu.find(0);
-    int root1 = dsu.find(1);
-    int root2 = dsu.find(2);
-    EXPECT_EQ(root0, root1);
-    EXPECT_NE(root0, root2);
-}
-
-TEST(DSUBasicTest, MultipleUnions) {
+TEST(DSU_Simple, UnionTwo) {
     DSU dsu(5);
     dsu.unite(0, 1);
-    dsu.unite(2, 3);
     EXPECT_EQ(dsu.find(0), dsu.find(1));
-    EXPECT_EQ(dsu.find(2), dsu.find(3));
     EXPECT_NE(dsu.find(0), dsu.find(2));
-    EXPECT_NE(dsu.find(1), dsu.find(4));
 }
-
-TEST(DSUBasicTest, TransitiveConnection) {
+TEST(DSU_Simple, ChainUnion) {
     DSU dsu(4);
     dsu.unite(0, 1);
     dsu.unite(1, 2);
     dsu.unite(2, 3);
     EXPECT_EQ(dsu.find(0), dsu.find(3));
-    EXPECT_EQ(dsu.find(1), dsu.find(3));
+    EXPECT_EQ(dsu.find(1), dsu.find(2));
 }
-
-TEST(DSUBasicTest, RankBehavior) {
-    DSU dsu(4);
-    dsu.unite(0, 1);
-    dsu.unite(2, 3);
-    int root1 = dsu.find(0);
-    int root2 = dsu.find(2);
-    dsu.unite(0, 2);
-    EXPECT_EQ(dsu.find(0), dsu.find(1));
-    EXPECT_EQ(dsu.find(0), dsu.find(2));
-    EXPECT_EQ(dsu.find(0), dsu.find(3));
-}
-
-TEST(DSUBasicTest, SelfUnion) {
+TEST(DSU_Simple, SelfUnion) {
     DSU dsu(3);
-    dsu.unite(1, 1); 
-    EXPECT_EQ(dsu.find(1), 1); 
+    dsu.unite(1, 1);  
+    EXPECT_EQ(dsu.find(1), 1);
 }
-
-TEST(DSUBasicTest, ChainUnion) {
+TEST(DSU_Simple, MultipleSets) {
     DSU dsu(6);
     dsu.unite(0, 1);
-    dsu.unite(1, 2);
     dsu.unite(2, 3);
-    dsu.unite(3, 4);
-    EXPECT_EQ(dsu.find(0), dsu.find(4));
-    EXPECT_NE(dsu.find(0), dsu.find(5));
+    dsu.unite(4, 5);
+    EXPECT_EQ(dsu.find(0), dsu.find(1));
+    EXPECT_EQ(dsu.find(2), dsu.find(3));
+    EXPECT_EQ(dsu.find(4), dsu.find(5));
+    EXPECT_NE(dsu.find(0), dsu.find(2));
+    EXPECT_NE(dsu.find(2), dsu.find(4));
 }
-
-TEST(DSUMemoryTest, LargeDSU) {
-    const int SIZE = 1000;
-    DSU dsu(SIZE);
-    for (int i = 2; i < SIZE; i += 2) {
+TEST(DSU_Simple, LargeDSU) {
+    DSU dsu(100);
+    for (int i = 1; i < 100; i++) {
         dsu.unite(0, i);
     }
-    for (int i = 3; i < SIZE; i += 2) {
-        dsu.unite(1, i);
+    for (int i = 0; i < 100; i++) {
+        EXPECT_EQ(dsu.find(i), dsu.find(0));
     }
-    EXPECT_EQ(dsu.find(0), dsu.find(2));
-    EXPECT_EQ(dsu.find(0), dsu.find(4));
-    EXPECT_EQ(dsu.find(1), dsu.find(3));
-    EXPECT_EQ(dsu.find(1), dsu.find(5));
-    EXPECT_NE(dsu.find(0), dsu.find(1));
 }
