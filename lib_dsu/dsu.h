abdc2865f8443568a33f1b3819550d2482797808
mrskristinaa <krispis2006@gmail.com>
2025-12-07 18:10:40 +0300

    corrected tests at DSU

--- lib_dsu/dsu.h
+++ lib_dsu/dsu.h
@@ -18,6 +18,7 @@ DSU::DSU(int size) : _size(size) {
 }
 DSU:: ~DSU() {
 	delete[] _parent;
+	delete[] _rank;
 }
 int DSU::find(int x) {
 	if (_parent[x] == x) {
--- tests/test_dsu.cpp
+++ tests/test_dsu.cpp
@@ -1,83 +1,47 @@
 #include <gtest/gtest.h>
 #include "dsu.h"
-TEST(DSUBasicTest, Initialization) {
+TEST(DSUTest, InitialState) {
     DSU dsu(5);
-    EXPECT_EQ(dsu.find(0), 0);
-    EXPECT_EQ(dsu.find(1), 1);
-    EXPECT_EQ(dsu.find(2), 2);
-    EXPECT_EQ(dsu.find(3), 3);
-    EXPECT_EQ(dsu.find(4), 4);
+    for (int i = 0; i < 5; i++) {
+        EXPECT_EQ(dsu.find(i), i);
+    }
 }
-
-TEST(DSUBasicTest, SimpleUnion) {
+TEST(DSUTest, SimpleUnion) {
     DSU dsu(3);
     dsu.unite(0, 1);
-    int root0 = dsu.find(0);
-    int root1 = dsu.find(1);
-    int root2 = dsu.find(2);
-    EXPECT_EQ(root0, root1);
-    EXPECT_NE(root0, root2);
-}
-
-TEST(DSUBasicTest, MultipleUnions) {
-    DSU dsu(5);
-    dsu.unite(0, 1);
-    dsu.unite(2, 3);
     EXPECT_EQ(dsu.find(0), dsu.find(1));
-    EXPECT_EQ(dsu.find(2), dsu.find(3));
     EXPECT_NE(dsu.find(0), dsu.find(2));
-    EXPECT_NE(dsu.find(1), dsu.find(4));
 }
-
-TEST(DSUBasicTest, TransitiveConnection) {
+TEST(DSUTest, TransitiveUnion) {
     DSU dsu(4);
     dsu.unite(0, 1);
     dsu.unite(1, 2);
-    dsu.unite(2, 3);
-    EXPECT_EQ(dsu.find(0), dsu.find(3));
-    EXPECT_EQ(dsu.find(1), dsu.find(3));
-}
-
-TEST(DSUBasicTest, RankBehavior) {
-    DSU dsu(4);
-    dsu.unite(0, 1);
-    dsu.unite(2, 3);
-    int root1 = dsu.find(0);
-    int root2 = dsu.find(2);
-    dsu.unite(0, 2);
-    EXPECT_EQ(dsu.find(0), dsu.find(1));
     EXPECT_EQ(dsu.find(0), dsu.find(2));
-    EXPECT_EQ(dsu.find(0), dsu.find(3));
+    EXPECT_NE(dsu.find(0), dsu.find(3));
 }
-
-TEST(DSUBasicTest, SelfUnion) {
-    DSU dsu(3);
-    dsu.unite(1, 1); 
-    EXPECT_EQ(dsu.find(1), 1); 
-}
-
-TEST(DSUBasicTest, ChainUnion) {
+TEST(DSUTest, MultipleUnions) {
     DSU dsu(6);
     dsu.unite(0, 1);
-    dsu.unite(1, 2);
     dsu.unite(2, 3);
-    dsu.unite(3, 4);
-    EXPECT_EQ(dsu.find(0), dsu.find(4));
-    EXPECT_NE(dsu.find(0), dsu.find(5));
+    dsu.unite(4, 5);
+    EXPECT_EQ(dsu.find(0), dsu.find(1));
+    EXPECT_EQ(dsu.find(2), dsu.find(3));
+    EXPECT_EQ(dsu.find(4), dsu.find(5));
+    EXPECT_NE(dsu.find(0), dsu.find(2));
+    EXPECT_NE(dsu.find(0), dsu.find(4));
+    EXPECT_NE(dsu.find(2), dsu.find(4));
 }
-
-TEST(DSUMemoryTest, LargeDSU) {
-    const int SIZE = 1000;
-    DSU dsu(SIZE);
-    for (int i = 2; i < SIZE; i += 2) {
-        dsu.unite(0, i);
+TEST(DSUTest, UnionAllElements) {
+    DSU dsu(5);
+    for (int i = 0; i < 4; i++) {
+        dsu.unite(i, i + 1);
     }
-    for (int i = 3; i < SIZE; i += 2) {
-        dsu.unite(1, i);
+    for (int i = 1; i < 5; i++) {
+        EXPECT_EQ(dsu.find(0), dsu.find(i));
     }
-    EXPECT_EQ(dsu.find(0), dsu.find(2));
-    EXPECT_EQ(dsu.find(0), dsu.find(4));
-    EXPECT_EQ(dsu.find(1), dsu.find(3));
-    EXPECT_EQ(dsu.find(1), dsu.find(5));
-    EXPECT_NE(dsu.find(0), dsu.find(1));
+}
+TEST(DSUTest, SelfUnion) {
+    DSU dsu(3);
+    dsu.unite(0, 0);
+    EXPECT_EQ(dsu.find(0), 0);
 }
